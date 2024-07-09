b3c141de567bf3587bc78196f5c5b8541dbdd128
Gerald <geraldjoly191@gmail.com>
2024-07-09 20:02:15 -0300

    Agregué la clase alumno que contiene ,atributo Nombre y Nota ,Sus metodos son getters y un ToString

---
+++ CAlumno.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace College_Eugene_de_Mazenod
+{
+    public class CAlumno
+    {
+        string nombre;
+        float nota ;
+
+        public CAlumno()
+        {
+            this.nombre ="undefined";
+            this.nota = 0;
+        }
+        public CAlumno(string nombre,float nota)
+        {
+            this.nombre = nombre;
+            this.nota = nota;
+        }
+
+        public float getNota()
+        {
+            return nota;
+        }
+        public string getNombre()
+        {
+                return nombre;
+        }
+
+
+        public override string ToString()
+        {
+            string datos = "\n Nombre :" + this.nombre + "\n Nota :" + this.nota.ToString();
+
+            return datos;
+        }
+
+        
+    }
+}
